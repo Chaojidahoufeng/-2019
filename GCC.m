3a5b9d607f7c77c5adb8a2de4373d30f637543e8
gaofan3 <gaofan1@163.com>
2019-12-02 22:58:21 +0800

    GCC add denoise and resample

--- GCC.m
+++ GCC.m
@@ -5,50 +5,75 @@ clear all; clc; close all;
 angle = zeros(1,14);
 speed = 343;
 distance = 0.1;
-for id = 2:14
-    [Input, Fs] = audioread(['./train/',num2str(id),'.wav']);
-    x1 = Input(:,1);
-    x2 = Input(:,2);
-    N=length(x1);  %长度
-    n=0:N-1;
+delta = 10;
+right_ans = [   8.8000000e+01
+   3.0000000e+01
+   1.7600000e+02
+   1.2800000e+02
+   9.0000000e+01
+   8.4000000e+01
+   1.0000000e+01
+   1.2200000e+02
+   7.0000000e+00
+   1.2000000e+01
+   9.3000000e+01
+   1.7000000e+01
+   1.4700000e+02
+   1.4700000e+02];
+for id = 1:14
+%     [Input, Fs] = audioread(['./train/',num2str(id),'.wav']);   %不使用降噪方法
+%     x1 = Input(:,1);
+%     x2 = Input(:,2);
+%     x1 = resample(x1, 100, 1);
+%     x2 = resample(x2, 100, 1);
+%     Fs = Fs*100;
+
+%     [~, Fs] = audioread(['./train/',num2str(id),'.wav']);  %使用谱减法降噪
+%     x1 = pujian(['./train/',num2str(id),'.wav'],1);
+%     x2 = pujian(['./train/',num2str(id),'.wav'],2);
+%     x1 = resample(x1, 100, 1);
+%     x2 = resample(x2, 100, 1);
+%     Fs = Fs*100;
+    
+    [~, Fs] = audioread(['./train/',num2str(id),'.wav']);  %使用高级方法降噪
+    x1 = denoise(['./train/',num2str(id),'.wav'],1);
+    x2 = denoise(['./train/',num2str(id),'.wav'],2);
+    x1 = resample(x1, 100, 1);
+    x2 = resample(x2, 100, 1);
+    Fs = Fs*100;
+    N = length(x1);  %长度
+    n = 0:N-1;
     t=n/Fs;   %时间序列
-    a1=5;     %信号幅度
-    a2=5;
-    d=2;     %延迟点数
-    % x1=a1*cos(2*pi*10*n/Fs);     %信号1
-    % x1=x1+randn(size(x1));      %加噪声
-    % x2=a2*cos(2*pi*10*(n+d)/Fs); %信号2
-    % x2=x2+randn(size(x2));
 %     subplot(211);
 %     plot(t,x1,'r');
-%     axis([-0.2 1.5 -6 6]);
 %     hold on;
 %     plot(t,x2,':');
-%     axis([-0.2 1.5 -6 6]);
 %     legend('x1信号', 'x2信号');
 %     xlabel('时间/s');ylabel('x1(t) x2(t)');
 %     title('原始信号');grid on;
 %     hold off
-    %% 互相关函数 1 
+    % 互相关函数 1 
     X1=fft(x1,2*N-1);
     X2=fft(x2,2*N-1);
     Sxy=X1.*conj(X2);
     Cxy=fftshift(ifft(Sxy));
-    %Cxy=fftshift(real(ifft(Sxy)));
 %     subplot(212);
-%     t1=(0:2*N-2)/Fs;                        %注意
+%     t1=(0:2*N-2)/Fs;    
 %     plot(t1,Cxy,'b');
 %     title('互相关函数');xlabel('时间/s');ylabel('Rx1x2(t)');grid on
     [~,location]=max(Cxy);%求出最大值max,及最大值所在的位置（第几行）location;
-    %d=location-N/2-1        %算出延迟了几个点
     d=location-N;
     Delay=d/Fs;              %求得时间延迟
     cos_angle = Delay*speed/distance;
     angle(id) = acosd(cos_angle);
+    angle(id)
 end
-
-%% 互相关函数 2
-% Cxy2=xcorr(x1,x2);    %Cross-correlation of the signal from-130 to 130 
-% %dd=find(Cxy2==max(Cxy2))-N     
-% [max1,location1]=max(Cxy2);
-% d2=location1-N
+% for id = 1:14  %修正
+%     if(imag(angle(id))>0)
+%         angle(id) = delta;
+%     elseif(imag(angle(id))<0)
+%         angle(id) = 180-delta;
+%     end
+% end
+error = real(angle) - right_ans';
+mean(abs(error));
